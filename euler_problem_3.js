83114a3b4b92344b83b2b710ff20ce232b64a111
Tarun <tnt.tarun@gmail.com>
2015-05-13 12:03:15 -0400

    Update euler_problem_3.js

--- euler_problem_3.js
+++ euler_problem_3.js
@@ -1,12 +1,16 @@
 //This is for finding factors of a number using a 'factor' function.//
-
+var factors = [];
 var factor = function (num1) {
-    for (var i=1; i<num1; i++) {
+    for (var i=2; i<num1; i++) {
         if (!(num1%i)) {
-            console.log(i);
+            factors.push(i);
         }
     }
+    console.log("The factors between 1 and the number are "+ factors);
+    console.log("The number of factors are " + factors.length)
 };
+
+factor (20);
 
 //This is for printing prime factors using a 'primef' function.//
 
